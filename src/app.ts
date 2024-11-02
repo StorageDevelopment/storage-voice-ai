1148d1bf5a075a253386e264ee002de8c6aec200
Alan Purugganan <alan.purugganan@gmail.com>
2024-11-01 23:21:45 -0400

    added default error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import exampleRoutes from './routes/exampleRoutes';
+import { Request, Response, NextFunction } from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/example', exampleRoutes);
+
+//default error handler
+app.use((err : any, req : Request, res : Response, next : NextFunction) => {
+    res.status(500).send(JSON.stringify(err));
+});
 
 export default app;
