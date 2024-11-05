efdfbffcba45e3559db05e9fdef90c48a759c78d
Alan Purugganan <alan.purugganan@gmail.com>
2024-11-04 23:50:14 -0500

    Updated test for getAllUnits

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -8,55 +8,74 @@ describe("Example Test Suite", () => {
     assert.strictEqual(1 + 1, 2);
   });
 
-  it("should return 200 OK", (done) => {
-    request(app).get("/api/example").expect(200, done);
+  it("should return 200 OK for getAllUnits", (done) => {
+    request(app)
+      .post("/api/tools")
+      .send({
+        "message": {
+          "timestamp": 0,
+          "type": "tool-calls",
+          "toolCalls": [
+            {
+              "id": "call_3JewUpif1jmC6YWLITvVEn7U",
+              "type": "function",
+              "function": {
+                "name": "getAllUnits",
+                "arguments": {
+                }
+              }
+            }
+          ]
+        }
+      })
+      .expect(200, done);
   });
 
-  it("should filter isRentable", (done) => {
-    request(app)
-      .get("/api/example?isRentable=true")
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        assert(res.body[0]._bRentable === "true");
-        done();
-      });
-  });
+  // it("should filter isRentable", (done) => {
+  //   request(app)
+  //     .get("/api/example?isRentable=true")
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       assert(res.body[0]._bRentable === "true");
+  //       done();
+  //     });
+  // });
 
-  it("should filter findBySize", (done) => {
-    request(app)
-      .get("/api/example?findBySize=15x10")
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        assert(res.body[0]._dcWidth === "10.0000");
-        assert(res.body[0]._dcLength === "15.0000");
-        done();
-      });
-  });
+  // it("should filter findBySize", (done) => {
+  //   request(app)
+  //     .get("/api/example?findBySize=15x10")
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       assert(res.body[0]._dcWidth === "10.0000");
+  //       assert(res.body[0]._dcLength === "15.0000");
+  //       done();
+  //     });
+  // });
 
-  it("should filter findByPrice", (done) => {
-    request(app)
-      .get("/api/example?findByPrice=[10,1000]")
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        assert(parseInt(res.body[0]._dcWebRate) <= 1000);
-        assert(parseInt(res.body[0]._dcWebRate) >= 10);
-        done();
-      });
-  });
+  // it("should filter findByPrice", (done) => {
+  //   request(app)
+  //     .get("/api/example?findByPrice=[10,1000]")
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       assert(parseInt(res.body[0]._dcWebRate) <= 1000);
+  //       assert(parseInt(res.body[0]._dcWebRate) >= 10);
+  //       done();
+  //     });
+  // });
 
-  it("should filter by multiple rules", (done) => {
-    request(app)
-      .get("/api/example?isRentable=true&findBySize=15x10")
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        assert(res.body[0]._dcWidth === "10.0000");
-        assert(res.body[0]._dcLength === "15.0000");
-        assert(res.body[0]._bRentable === "true");
-        done();
-      });
-  });
+  // it("should filter by multiple rules", (done) => {
+  //   request(app)
+  //     .get("/api/example?isRentable=true&findBySize=15x10")
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       if (err) return done(err);
+  //       assert(res.body[0]._dcWidth === "10.0000");
+  //       assert(res.body[0]._dcLength === "15.0000");
+  //       assert(res.body[0]._bRentable === "true");
+  //       done();
+  //     });
+  // });
 });
