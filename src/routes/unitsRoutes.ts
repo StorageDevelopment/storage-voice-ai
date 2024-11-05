76c0a19d157d2851a7ad7dba11a7677fa33dfff2
Alan Purugganan <alan.purugganan@gmail.com>
2024-11-04 23:49:55 -0500

    removed unitsRoute, unitsController, and unitsMiddleware

--- src/routes/unitsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getExample } from '../controllers/exampleController';
-import { unitsController } from '../controllers/unitsController';
-
-const router = Router();
-
-router.post('/', unitsController);
-
-export default router;
