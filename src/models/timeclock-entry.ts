4548fbd64324bd325e5c34977afdb6e08d096276
Alan Purugganan <alan.purugganan@gmail.com>
2025-02-08 10:43:18 -0500

    added timeclock entry

---
+++ src/models/timeclock-entry.ts
@@ -0,0 +1,47 @@
+export class TimeclockEntry {
+  private type: string;
+  private timestamp: string;
+  private latitude: number | null;
+  private longitude: number | null;
+
+  constructor(data: any){
+    this.type = data.type ?? '';
+    this.timestamp = data.timestamp ?? '';
+    this.latitude = data.latitude ?? null;
+    this.longitude = data.longitude ?? null
+  }
+
+  // Getters
+  public getType(): string {
+    return this.type;
+  }
+
+  public getTimestamp(): string {
+    return this.timestamp;
+  }
+
+  public getLatitude(): number | null {
+    return this.latitude;
+  }
+
+  public getLongitude(): number | null {
+    return this.longitude;
+  }
+
+  // Setters
+  public setType(type: string): void {
+    this.type = type;
+  }
+
+  public setTimestamp(timestamp: string): void {
+    this.timestamp = timestamp;
+  }
+
+  public setLatitude(latitude: number | null): void {
+    this.latitude = latitude;
+  }
+
+  public setLongitude(longitude: number | null): void {
+    this.longitude = longitude;
+  }
+}
