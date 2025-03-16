b454c8d4e5093f2f8c365470bddaad47eb584f0a
Alan Purugganan <alan.purugganan@gmail.com>
2025-03-16 10:17:20 -0400

    added gracefully shutdown stub

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
+import http from 'http';
 import app from './app';
+
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// Create HTTP server
+const server = http.createServer(app);
+
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const server = app;
+// Graceful shutdown
+const gracefulShutdown = () => {
+  console.log('Received kill signal, shutting down gracefully...');
+  server.close(() => {
+    console.log('Closed out remaining connections.');
+    process.exit(0);
+  });
+
+  // Forcefully shut down after 10 seconds
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 10000);
+};
+
+// Listen for termination signals
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
 
 export default server;
